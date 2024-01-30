3714c151ab0bb4f684eabc4622e524da397fe507
gokullal <gokullal29112000@gmail.com>
2024-01-30 20:24:19 +0000

    sticky dynamic text

--- script.js
+++ script.js
@@ -6,38 +6,43 @@ function sticky_relocate_2() {
     // About Section
     var about_top = $('#about').offset().top;
     var about_end = $('#about .end').offset().top - 100;
-    if (window_top >= about_top && window_top < about_end) {
+    if (window_top >= about_top ) {
         $('#about .mobile-nav').addClass('sticky');
-    } else {
+    } 
+    else {
         $('#about .mobile-nav').removeClass('sticky');
+        $('#about .mobile-nav h3').text('About');
     }
 
     // Relevant Experience Section
     var rel_experience_top = $('#rel-experience').offset().top;
     var rel_experience_end = $('#rel-experience .end').offset().top - 100;
     if (window_top >= rel_experience_top && window_top < rel_experience_end) {
-        $('#rel-experience .mobile-nav').addClass('sticky');
-    } else {
-        $('#rel-experience .mobile-nav').removeClass('sticky');
-    }
+        $('#about .mobile-nav h3').text('Relavent Experience');
+    } 
+    // else {
+    //     $('#rel-experience .mobile-nav').removeClass('sticky');
+    // }
 
     // Other Experience Section
     var other_experience_top = $('#other-experience').offset().top;
     var other_experience_end = $('#other-experience .end').offset().top - 100;
     if (window_top >= other_experience_top && window_top < other_experience_end) {
-        $('#other-experience .mobile-nav').addClass('sticky');
-    } else {
-        $('#other-experience .mobile-nav').removeClass('sticky');
-    }
+        $('#about .mobile-nav h3').text('Other Experience');
+} 
+    // else {
+    //     $('#other-experience .mobile-nav').removeClass('sticky');
+    // }
 
     // Certifications Section
     var certifications_top = $('#certifications').offset().top;
     var certifications_end = $('#certifications .end').offset().top - 100;
     if (window_top >= certifications_top && window_top < certifications_end) {
-        $('#certifications .mobile-nav').addClass('sticky');
-    } else {
-        $('#certifications .mobile-nav').removeClass('sticky');
-    }
+        $('#about .mobile-nav h3').text('Certifications');
+    } 
+    // else {
+    //     $('#certifications .mobile-nav').removeClass('sticky');
+    // }
 }
 
 $(window).scroll(sticky_relocate_2);
